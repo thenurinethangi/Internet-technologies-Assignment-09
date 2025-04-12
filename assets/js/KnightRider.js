a4de7080b358dc2668a99e53651f6bea64433354
thenurinethangi <thenurinathangi@gmail.com>
2025-04-12 19:42:20 +0530

    Update project with recent changes

--- assets/js/KnightRider.js
+++ assets/js/KnightRider.js
@@ -1,12 +1,12 @@
-const boxes = document.querySelectorAll('section > div');
+const $boxes = $('section > div');
 const colorList = ['#FFCDD2', '#EF9A9A', '#E57373', '#EF5350'];
 let isRunning = false;
 
-const startBtn = document.getElementById('start-btn');
-const stopBtn = document.getElementById('stop-btn');
+const $startBtn = $('#start-btn');
+const $stopBtn = $('#stop-btn');
 
-startBtn.addEventListener('click', animationStart);
-stopBtn.addEventListener('click', animationStop);
+$startBtn.on('click', animationStart);
+$stopBtn.on('click', animationStop);
 
 function animationStart() {
     if (isRunning) return;
@@ -14,47 +14,45 @@ function animationStart() {
 
     let currentRedBox = 4;
 
-    for (let i = 0; i < boxes.length; i++) {
-        if (boxes[i].style.backgroundColor === '#E53935') {
+    $boxes.each(function(i) {
+        if ($(this).css('background-color') === 'rgb(229, 57, 53)') {
             currentRedBox = i;
-            break;
+            return false;
         }
-    }
+    });
 
     for (let i = 0; i < 4; i++) {
-        boxes[i].style.backgroundColor = colorList[i];
+        $boxes.eq(i).css('background-color', colorList[i]);
     }
 
     let nextBox = currentRedBox;
     looping(nextBox);
 }
 
-
 function animationStop() {
     console.log("stop");
     isRunning = false;
 }
 
-
 async function looping(nextBox) {
     while (isRunning) {
         nextBox++;
 
-        if (nextBox < boxes.length) {
-            boxes[nextBox].style.backgroundColor = '#E53935';
+        if (nextBox < $boxes.length) {
+            $boxes.eq(nextBox).css('background-color', '#E53935');
 
             let x = 3;
             let y = nextBox;
             for (let i = nextBox - 1; i >= nextBox - 4; i--) {
                 if (i >= 0) {
-                    boxes[i].style.backgroundColor = colorList[x];
+                    $boxes.eq(i).css('background-color', colorList[x]);
                     x--;
                     y = i;
                 }
             }
 
             if (y - 1 >= 0) {
-                boxes[y - 1].style.backgroundColor = '#FFFFFF';
+                $boxes.eq(y - 1).css('background-color', '#FFFFFF');
             }
 
         } else {
@@ -64,20 +62,20 @@ async function looping(nextBox) {
                 curentRedBox--;
 
                 if (curentRedBox >= 0) {
-                    boxes[curentRedBox].style.backgroundColor = '#E53935';
+                    $boxes.eq(curentRedBox).css('background-color', '#E53935');
 
                     let x = 3;
                     let a = curentRedBox;
                     for (let i = curentRedBox + 1; i < curentRedBox + 5; i++) {
-                        if (i < boxes.length) {
-                            boxes[i].style.backgroundColor = colorList[x];
+                        if (i < $boxes.length) {
+                            $boxes.eq(i).css('background-color', colorList[x]);
                             x--;
                             a = i;
                         }
                     }
 
-                    if (a + 1 < boxes.length) {
-                        boxes[a + 1].style.backgroundColor = '#FFFFFF';
+                    if (a + 1 < $boxes.length) {
+                        $boxes.eq(a + 1).css('background-color', '#FFFFFF');
                     }
                 } else {
                     nextBox = 3;
@@ -91,7 +89,6 @@ async function looping(nextBox) {
         await delay(100);
     }
 }
-
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
